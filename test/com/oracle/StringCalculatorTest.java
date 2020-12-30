9903c3dbe6308150387abfdef6a68cbe628502ff
harshiash <harshiiash2008@gmail.com>
2020-12-30 12:21:14 +0530

    Two integers are added

--- test/com/oracle/StringCalculatorTest.java
+++ test/com/oracle/StringCalculatorTest.java
@@ -26,4 +26,8 @@ public class StringCalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, StringCalculator.add("1"));
 	}
+        @Test
+	public void testTwoNumber(){
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
 }
