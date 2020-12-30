e4e4f4b7758c049a4be3b1097e0da4a1efad3069
harshiash <harshiiash2008@gmail.com>
2020-12-30 12:17:47 +0530

    One integer added

--- test/com/oracle/StringCalculatorTest.java
+++ test/com/oracle/StringCalculatorTest.java
@@ -22,4 +22,8 @@ public class StringCalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, StringCalculator.add(""));
 	}
+        @Test
+	public void testOneNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
 }
